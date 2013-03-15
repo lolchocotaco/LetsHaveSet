4f9cc6c59d48a0b1235d6ad8bc4be964695aaf80
Sameer <schauhan19@gmail.com>
2013-03-15 02:30:19 -0400

    Random Button Addition

--- src/gui/AppWindow.java
+++ src/gui/AppWindow.java
@@ -6,6 +6,11 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class AppWindow {
 
@@ -40,12 +45,17 @@ public class AppWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 318, 205);
+		frame.setBounds(100, 100, 572, 461);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Le Button");
-		btnNewButton.setBounds(39, 93, 175, 67);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("You Clicked Me");
+			}
+		});
+		btnNewButton.setBounds(183, 166, 175, 67);
 		frame.getContentPane().add(btnNewButton);
 		
 		txtWhat = new JTextField();
@@ -53,5 +63,17 @@ public class AppWindow {
 		txtWhat.setBounds(79, 46, 86, 20);
 		frame.getContentPane().add(txtWhat);
 		txtWhat.setColumns(10);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNew = new JMenuItem("New");
+		mnFile.add(mntmNew);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnFile.add(mntmOpen);
 	}
 }
