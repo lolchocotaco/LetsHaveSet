7d01ed78047f1ec8e360b7598a215014a425385a
Vasily <vasilydk@gmail.com>
2013-04-03 17:07:21 -0400

    Created MainClient file.

---
+++ src/gameLogic/MainClient.java
@@ -0,0 +1,32 @@
+package gameLogic;
+
+
+import gui.AppWindow;
+import gui.LoginWindow;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainClient {
+	
+	public static void main(String[] args) {
+		
+		try {
+			Socket clientSocket = new Socket("sable10.ee.cooper.edu", 5342);
+		} catch (UnknownHostException e) {
+			System.err.println("Unknown Host Exception thrown!");
+			System.exit(-1);
+		} catch (IOException e) {
+			System.err.println("Error connecting to Server!");
+			System.exit(-1);
+		}
+		
+		LoginWindow loginWindow = new LoginWindow();
+		AppWindow appWindow = new AppWindow();
+		
+	 loginWindow.frmLogin.setVisible(true);
+		
+	}
+
+}
